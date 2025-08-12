83cc8ffb5c513a5f80ac2cc08e2424545c42411b
mariozip <marius1950@gmail.com>
2025-08-12 17:04:44 +0000

    Inizio di test Python

---
+++ main.py
@@ -0,0 +1,8 @@
+def saluta(nome):
+    print(f"Ciao, {nome}! Benvenuto in Codespaces.")
+
+saluta("Studente Python")
+
+lista_numeri = [1, 2, 3, 4, 5]
+quadrati = [n**2 for n in lista_numeri]
+print(f"I quadrati dei numeri sono: {quadrati}")
